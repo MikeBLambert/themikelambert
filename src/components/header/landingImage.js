6f07cebace280887a92e1e17ff8c7f67f2560487
mlamb <mlamb128@gmail.com>
2018-12-23 20:22:29 -0800

    Uppercase component

--- src/components/header/landingImage.js
+++ src/components/header/landingImage.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import backgroundImage from '../../images/landing-image.jpg';
-import styles from './landingImage.module.css';
+import styles from './LandingImage.module.css';
 
-export default function landingImage() {
+export default function LandingImage() {
   return (
     <div id={styles.landing}>
       <img src={backgroundImage} alt='mountains' id={styles.landingImage} />
