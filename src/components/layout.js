5972ed35226ad84a175f33a044b3535b1c4bb256
mlamb <mlamb128@gmail.com>
2018-12-24 10:25:25 -0800

    Fix layout to remove all parent components and add delay to animation on scroll

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,12 +6,10 @@ import LandingImage from './header/LandingImage';
 import styles from './layout.module.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import About from './about/About';
-import Projects from './projects/Projects';
-import Blog from './blog/Blog';
-import Contact from './contact/Contact';
 
-AOS.init();
+AOS.init({
+  delay: 300,
+});
 
 const Layout = () => (
   <StaticQuery
@@ -25,22 +23,10 @@ const Layout = () => (
       }
     `}
     render={data => (
-      <div>
+      <>
         <LandingImage id={styles.landingWithHeader} />
         <Nav siteTitle={data.site.siteMetadata.title} />
-        <About />
-        <Projects />
-        <Blog />
-        <Contact />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        />
-      </div>
+      </>
     )}
   />
 );
