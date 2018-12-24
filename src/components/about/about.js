d3760052ce576f6c1b084e224a4f475dcc9acd50
mlamb <mlamb128@gmail.com>
2018-12-23 20:19:12 -0800

    Add About component

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import Code from './code'
-import styles from './about.module.css'
-import Study from './study';
-import Global from './global';
+import Code from './Code'
+import styles from './About.module.css'
+import Study from './Study';
+import Global from './Travel';
 
-export default function about() {
+export default function About() {
   return (
-    <div id={styles.container}>
+    <div className={styles.aboutContainer} id="About">
+    <h1 id={styles.aboutTitle} data-aos="fade-in">
+          {'< About />'}
+        </h1>
       <Code />
       <Study />
       <Global />
