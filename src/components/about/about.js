10bbf12fe4ecc52dfa595c8b14b1cc0a88906fd3
mlamb <mlamb128@gmail.com>
2018-12-25 13:57:32 -0800

    prettier

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import Code from './Code'
-import styles from './About.module.css'
+import React from 'react';
+import Code from './Code';
+import styles from './About.module.css';
 import Study from './Study';
 import Global from './Travel';
 
 export default function About() {
   return (
     <div className={styles.aboutContainer} id="About">
-    <h1 id={styles.aboutTitle} data-aos="fade-in">
-          {'< About />'}
-        </h1>
+      <h1 id={styles.aboutTitle} data-aos="fade-in">
+        {'< About />'}
+      </h1>
       <Code />
       <Study />
       <Global />
     </div>
-  )
+  );
 }
