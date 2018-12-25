0b72f9fba4989f7eb3a5dbb02509073083b2213b
mlamb <mlamb128@gmail.com>
2018-12-25 13:56:34 -0800

    Update gatsby-config to add new title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: '',
-    description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@gatsbyjs',
+    title: 'Mike Lambert',
+    description: 'Mike Lambert\'s personal websit',
+    author: 'Mike Lambert',
   },
   plugins: [
     'gatsby-plugin-typography',
