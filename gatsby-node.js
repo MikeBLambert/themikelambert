c6264d550179218aa71a56168ba192e20a182b6c
mlamb <mlamb128@gmail.com>
2018-12-25 21:16:13 -0800

    Add AWS amplify

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,20 +7,20 @@
 // You can delete this file if you're not using it
 // const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === "build-html") {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            test: /aos.js/,
+// exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+//   if (stage === "build-html") {
+//     actions.setWebpackConfig({
+//       module: {
+//         rules: [
+//           {
+//             test: /aos.js/,
             // loader: 'style-loader'
             // loader: MiniCssExtractPlugin.loader,
 
-            use: loaders.null(),
-          },
-        ],
-      },
-    })
-  }
-}
+            // use: loaders.null(),
+//           },
+//         ],
+//       },
+//     })
+//   }
+// }
